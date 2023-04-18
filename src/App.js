15f654bbc91146099fad1d48bfe748c61f3cf964
evelyn <evelynpistoia@gmail.com>
2023-04-18 14:34:00 -0300

    modificaciones API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
 import { ItemCount } from './components/ItemCount/ItemCount';
+import{Pika} from './components/Pika/Pika';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar/>
       <ItemListContainer saludo="Hola mundo"/>
       <ItemCount/>
+      <Pika/>
     </div>
     </>
   );
